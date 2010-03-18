bf9b3f7a8915df360510748e4133a2e6c9af1431
jeremy <jeremy@rightscale.com>
2010-03-18 09:31:35 -0700

    Refs #6294 - adding HOME environment variable setting to rnac.rb so it can startup the instance agent with this environment if it is missing.

--- scripts/rnac.rb
+++ scripts/rnac.rb
@@ -4,6 +4,10 @@
 #
 # See lib/agent_controller.rb for additional information.
 
+# Monit cleans the environment before running a daemon.  
+# We can re-set any necessary environment here.
+ENV['HOME'] = "/root" unless ENV['HOME']
+
 THIS_DIR = File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__)
 $:.push(File.join(THIS_DIR, 'lib'))
 
